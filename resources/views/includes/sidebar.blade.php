475e30ffe562e230b99f262e10c37230bb19e0e8
kevinwiwaha <kevinwiwaha@gmail.com>
2020-09-01 20:20:20 +0700

    base + admin

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,55 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-plane-departure"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Holidesia<sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-images"></i>
+            <span>Galeri Travel</span></a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-route"></i>
+            <span>Paket Travel</span></a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-credit-card"></i>
+            <span>Transaksi</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+
+
+
+
+
+
+
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
