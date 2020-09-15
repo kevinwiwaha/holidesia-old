bdefb7210f015416781be175423079eabfd8eb47
kevinwiwaha <kevinwiwaha@gmail.com>
2020-09-15 07:42:47 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::prefix('admin')->namespace('Admin')->group(function () {
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
